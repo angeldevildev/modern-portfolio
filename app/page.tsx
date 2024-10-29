5c4cd99fce560a430c6fc57c95870785b6fe0997
mineshopdev <mineshop47@gmail.com>
2024-10-29 17:30:11 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
-import Contact from "./components/Contact";
+//import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { Analytics } from "@vercel/analytics/react"
 
